92ecc7a0110b1aac18e64a2678649d7909785082
ydhan-mac-pro <ydhan1024@gmail.com>
2025-02-04 21:22:46 +0900

    Telepathy based unity client prototype

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+    private Telepathy.Client client;
+    private void Awake(){
+        Application.runInBackground = true;
+
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this);
+        }
+    }
+    
+    void Start(){
+        client = new Telepathy.Client(1024);
+        client.OnConnected = () => Debug.Log("Client Connected");
+        client.OnDisconnected = () => Debug.Log("Client Disconnected");
+        client.Connect("localhost", 13);
+    }
+
+    void Update(){
+        client.Tick(100);
+    }
+
+    void OnApplicationQuit(){
+        client.Disconnect();
+    }
+}
