d8efeeb22301dd134e7c2f122904974a0c63c07e
ydhan-mac-pro <ydhan1024@gmail.com>
2025-02-11 15:08:43 +0900

    final prototype

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+
+#if UNITY_WEBGL
+//Unity WebGL Code
+
+#else
 
 public class NetworkManager : MonoBehaviour
 {
     public static NetworkManager instance;
+
     private Telepathy.Client client;
 
     private void Awake(){
@@ -21,20 +28,73 @@ public class NetworkManager : MonoBehaviour
     }
     
     void Start(){
+
         client = new Telepathy.Client(1024);
         
-        client.OnConnected = () => Debug.Log("Client Connected");
+        client.OnConnected = () => 
+        {
+            Debug.Log("Client Connected");
+            
+            /*
+            List<byte> _packet = new List<byte>();
+            _packet.AddRange(BitConverter.GetBytes((ushort)0x0000));
+            client.Send(new ArraySegment<byte>(_packet.ToArray()));
+            */
+            
+            
+            /*
+            List<byte> _packet = new List<byte>();
+            _packet.AddRange(BitConverter.GetBytes((ushort)0x0001));
+            string str1 = "test.txt\0";
+            _packet.AddRange(BitConverter.GetBytes((int)str1.Length));
+            _packet.AddRange(Encoding.UTF8.GetBytes(str1));
+            string str2 = "hello everyone. goodnight";
+            _packet.AddRange(BitConverter.GetBytes((int)str2.Length));
+            _packet.AddRange(Encoding.UTF8.GetBytes(str2));
+            client.Send(new ArraySegment<byte>(_packet.ToArray()));
+            */
+
+            /*
+            string str = @"
+            {
+                ""levelName"" : ""test7"",
+                ""data"" : ""datass oh yeah""
+            }";
+            List<byte> _packet = new List<byte>();
+            _packet.AddRange(BitConverter.GetBytes((ushort)0x0000));
+            _packet.AddRange(BitConverter.GetBytes((int)str.Length));
+            _packet.AddRange(Encoding.UTF8.GetBytes(str));
+            client.Send(new ArraySegment<byte>(_packet.ToArray()));
+            Debug.Log(str.Length);
+            */
+
+            
+            List<byte> _packet = new List<byte>();
+            _packet.AddRange(BitConverter.GetBytes((ushort)0x0001));
+            client.Send(new ArraySegment<byte>(_packet.ToArray()));
+            
+            
+            _packet = new List<byte>();
+            _packet.AddRange(BitConverter.GetBytes((ushort)0x0002));
+            _packet.AddRange(BitConverter.GetBytes((int)1));
+            client.Send(new ArraySegment<byte>(_packet.ToArray()));
+        };
         client.OnData = PacketHandler.HandleData;
         client.OnDisconnected = () => Debug.Log("Client Disconnected");
 
-        client.Connect("localhost", 13);
+        //client.Connect("localhost", 13);
+        //client.Connect("192.168.0.120", 12345);
+        client.Connect("118.42.117.50", 12345);
     }
 
     void Update(){
         client.Tick(100);
     }
 
-    void OnApplicationQuit(){
-        client.Disconnect();
+    void OnDestroy(){
+        Debug.Log("disconnect");
+        if(client.Connected)
+            client.Disconnect();
     }
 }
+#endif
--- Assets/Scripts/PacketHandler.cs
+++ Assets/Scripts/PacketHandler.cs
@@ -11,8 +11,22 @@ public class PacketHandler
     static PacketHandler(){
         packetHandlers = new Dictionary<ushort, Handler>();
 
-        packetHandlers.Add(0x0000, (ArraySegment<byte> _packet) => {Debug.Log(Encoding.UTF8.GetString(_packet.Array, 2+_packet.Offset, _packet.Count));});
-    }
+        //packetHandlers.Add(0x0000, (ArraySegment<byte> _packet) => {Debug.Log(Encoding.UTF8.GetString(_packet.Array, 2+_packet.Offset, _packet.Count));});
+        packetHandlers.Add(0x0001, (ArraySegment<byte> _packet) => {
+            int _size = BitConverter.ToInt32(_packet.Array, 2+_packet.Offset);
+            string _json = Encoding.UTF8.GetString(_packet.Array, 6+_packet.Offset, _size);
+            Debug.Log(_json);
+        });
+
+        packetHandlers.Add(0x0002, (ArraySegment<byte> _packet) => {
+            int _id = BitConverter.ToInt32(_packet.Array, 2+_packet.Offset);
+            int _size = BitConverter.ToInt32(_packet.Array, 6+_packet.Offset);
+            string _json = Encoding.UTF8.GetString(_packet.Array, 10+_packet.Offset, _size);
+            Debug.Log(_id);
+            Debug.Log(_json);
+        });
+
+   }
 
     public static void HandleData(ArraySegment<byte> data){
         Debug.Log("Handling Data");
